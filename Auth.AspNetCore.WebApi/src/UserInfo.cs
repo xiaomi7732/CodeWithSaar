6d3989134d7fb97b0fa5194f64eb6834077eb4ef
Saar Shen 🚾 <saars@microsoft.com>
2021-03-09 17:06:04 -0800

    Supports additional claims

--- Auth.AspNetCore.WebApi/src/UserInfo.cs
+++ Auth.AspNetCore.WebApi/src/UserInfo.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace JWTAuth.AspNetCore.WebAPI
 {
@@ -9,5 +11,10 @@ namespace JWTAuth.AspNetCore.WebAPI
     {
         public string Name { get; set; }
         public IEnumerable<string> Roles { get; set; }
+
+        public IEnumerable<Claim> AdditionalClaims { get; } = Enumerable.Empty<Claim>();
+
+        public void AppendAdditionalClaims(params Claim[] claims)
+            => AdditionalClaims.Union(claims);
     }
 }
