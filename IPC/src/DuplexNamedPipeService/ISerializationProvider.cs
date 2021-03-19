8231134de897eec6bf032c99888f2458946c9222
Saar Shen 🚾 <saars@microsoft.com>
2021-03-18 21:03:29 -0700

    Add a comment for serialization provider interface

--- IPC/src/DuplexNamedPipeService/ISerializationProvider.cs
+++ IPC/src/DuplexNamedPipeService/ISerializationProvider.cs
@@ -1,5 +1,9 @@
 namespace CodeWithSaar.IPC
 {
+    /// <summary>
+    /// An extention point to allow different serializers.
+    /// Check the unit test of 'DefaultSerializationProviderTests' to see the requirement for implementations.
+    /// </summary>
     public interface ISerializationProvider
     {
         bool TrySerialize<T>(T payload, out string serialized);
