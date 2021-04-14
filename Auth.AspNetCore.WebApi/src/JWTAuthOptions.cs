eff8c95a7bea545dfa76c977ae23ac769a9ef540
Saar Shen 🚾 <saars@microsoft.com>
2021-04-13 17:41:20 -0700

    WIP: Appending property for user / role validation

--- Auth.AspNetCore.WebApi/src/JWTAuthOptions.cs
+++ Auth.AspNetCore.WebApi/src/JWTAuthOptions.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using JWTAuth.AspNetCore.WebAPI;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
 
@@ -22,5 +25,9 @@ namespace Microsoft.Extensions.DependencyInjection
         public PathString TokenPath { get; set; } = "/token";
 
         public Func<MessageReceivedContext, Task> OnJWTAuthenticationMessageReceived { get; set; }
+
+        public Func<string, Task<UserInfo>> ValidateUserInfo { get; set; } = token => Task.FromResult<UserInfo>(null);
+
+        public Func<UserInfo, Task<IEnumerable<string>>> ValidateRoleInfo { get; set; } = user => Task.FromResult(Enumerable.Empty<string>());
     }
 }
