3ac2e491f223efd2c734429b28727d4f1f4d037f
Saar Shen <xiaomi7732@hotmail.com>
2021-04-28 16:11:52 -0700

    Build up the services

---
+++ Auth.AspNetCore.WebApi/example-sqldb/Services/RoleService.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.Example.WithSQLDB
+{
+    public class RoleService : UserDBBase
+    {
+        public RoleService(UserDBContext userDBContext) : base(userDBContext)
+        {
+
+        }
+
+        public async Task AddRoleAsync(string roleName, CancellationToken cancellationToken = default)
+        {
+            if (roleName is null)
+            {
+                throw new System.ArgumentNullException(nameof(roleName));
+            }
+            await UserDBContext.Roles.AddAsync(new Role() { Name = roleName }, cancellationToken).ConfigureAwait(false);
+            await UserDBContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task AddRoleAssignmentAsync(string userName, string roleName, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException($"'{nameof(userName)}' cannot be null or empty.", nameof(userName));
+            }
+
+            if (string.IsNullOrEmpty(roleName))
+            {
+                throw new ArgumentException($"'{nameof(roleName)}' cannot be null or empty.", nameof(roleName));
+            }
+
+            User targetUser = null;
+            try
+            {
+
+                targetUser = GetFirstUserByName(userName);
+                if (targetUser is null)
+                {
+                    throw new InvalidOperationException($"Target user {userName} is not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            Role targetRole = GetFirstRoleByName(roleName);
+            if (targetRole is null)
+            {
+                throw new InvalidOperationException($"Target role {roleName} is not found.");
+            }
+
+            if (targetUser.Roles.FirstOrDefault(r => string.Equals(r.Name, targetRole.Name)) is null)
+            {
+                targetUser.Roles.Add(targetRole);
+            }
+            await UserDBContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public Task DisableRoleAsync(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers(Role role)
+        {
+            Role targetRole = await UserDBContext.Roles.Include(r => r.Users).SingleAsync(r => r.Id == role.Id);
+            return targetRole.Users;
+        }
+
+        private User GetFirstUserByName(string userName)
+            => UserDBContext.Users.Include(u => u.Roles).SingleOrDefault(u => string.Equals(u.Name, userName));
+
+        private Role GetFirstRoleByName(string roleName)
+            => UserDBContext.Roles.SingleOrDefault(r => string.Equals(r.Name, roleName));
+    }
+}
--- Auth.AspNetCore.WebApi/example-sqldb/Services/UserService.cs
+++ Auth.AspNetCore.WebApi/example-sqldb/Services/UserService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -8,16 +9,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace JWT.Example.WithSQLDB
 {
-    public class UserService
+    public class UserService : UserDBBase
     {
-        private readonly UserDBContext _userDatabase;
-
         public UserService(UserDBContext userDatabase)
+            : base(userDatabase)
         {
-            _userDatabase = userDatabase ?? throw new System.ArgumentNullException(nameof(userDatabase));
         }
 
-        public async Task CreateAsync(string userName, string password, CancellationToken cancellationToken = default)
+        public async Task AddUserAsync(string userName, string password, CancellationToken cancellationToken = default)
         {
             if (string.IsNullOrEmpty(userName))
             {
@@ -33,65 +32,24 @@ namespace JWT.Example.WithSQLDB
             using Stream passwordStream = password.ToStream();
             byte[] passwordHash = await sha256.ComputeHashAsync(passwordStream).ConfigureAwait(false);
 
-            _userDatabase.Add<User>(new User { Name = userName, PasswordHash = passwordHash });
-            await _userDatabase.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            UserDBContext.Add<User>(new User { Name = userName, PasswordHash = passwordHash });
+            await UserDBContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
 
-        public async Task CreateRoleAsync(string roleName, CancellationToken cancellationToken = default)
+        public IAsyncEnumerable<User> ListUsersAsync(CancellationToken cancellationToken = default)
         {
-            if (roleName is null)
-            {
-                throw new System.ArgumentNullException(nameof(roleName));
-            }
-            await _userDatabase.Roles.AddAsync(new Role() { Name = roleName }, cancellationToken).ConfigureAwait(false);
-            await _userDatabase.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            return UserDBContext.Users.AsAsyncEnumerable<User>();
         }
 
-        public async Task AddRoleAssignmentAsync(string userName, string roleName, CancellationToken cancellationToken = default)
+        public Task ChangePasswordAsync(User user, CancellationToken cancellationToken = default)
         {
-            if (string.IsNullOrEmpty(userName))
-            {
-                throw new ArgumentException($"'{nameof(userName)}' cannot be null or empty.", nameof(userName));
-            }
-
-            if (string.IsNullOrEmpty(roleName))
-            {
-                throw new ArgumentException($"'{nameof(roleName)}' cannot be null or empty.", nameof(roleName));
-            }
-
-            User targetUser = null;
-            try
-            {
-
-                targetUser = GetFirstUserByName(userName);
-                if (targetUser is null)
-                {
-                    throw new InvalidOperationException($"Target user {userName} is not found.");
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine(ex.ToString());
-                throw;
-            }
-
-            Role targetRole = GetFirstRoleByName(roleName);
-            if (targetRole is null)
-            {
-                throw new InvalidOperationException($"Target role {roleName} is not found.");
-            }
-
-            if (targetUser.Roles.FirstOrDefault(r => string.Equals(r.Name, targetRole.Name)) is null)
-            {
-                targetUser.Roles.Add(targetRole);
-            }
-            await _userDatabase.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            throw new NotImplementedException();
         }
 
-        private User GetFirstUserByName(string userName)
-            => _userDatabase.Users.Include(u => u.Roles).SingleOrDefault(u => string.Equals(u.Name, userName));
-
-        private Role GetFirstRoleByName(string roleName)
-            => _userDatabase.Roles.SingleOrDefault(r => string.Equals(r.Name, roleName));
+        public async Task<IEnumerable<Role>> GetRoles(User user)
+        {
+            User targetUser = await UserDBContext.Users.Include(u => u.Roles).SingleAsync(u => u.Id == user.Id);
+            return targetUser.Roles;
+        }
     }
 }
