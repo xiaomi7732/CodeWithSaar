ece80e6d429f6baaec1126309cc063b91158e709
Saar Shen <xiaomi7732@hotmail.com>
2021-04-29 18:44:26 -0700

    Add authorization

--- Auth.AspNetCore.WebApi/example-sqldb/Controllers/WeatherForecastController.cs
+++ Auth.AspNetCore.WebApi/example-sqldb/Controllers/WeatherForecastController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace JWT.Example.WithSQLDB.Controllers
 {
+    [Authorize(Roles = "Admin, User")]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
