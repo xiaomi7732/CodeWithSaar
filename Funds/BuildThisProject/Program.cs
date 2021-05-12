f21adbd151e9a8cc316d72812a1232c44f2bfb2a
Saar Shen <xiaomi7732@hotmail.com>
2021-05-11 21:12:39 -0700

    Add an example of build netcore project

---
+++ Funds/BuildThisProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMyFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World!");
+        }
+    }
+}
