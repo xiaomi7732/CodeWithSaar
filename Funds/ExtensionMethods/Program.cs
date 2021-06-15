3662af20104f98c5cfac4bd56d283f71283ca49d
Saar Shen <xiaomi7732@hotmail.com>
2021-06-14 19:57:25 -0700

    Initiailize extension method example

---
+++ Funds/ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            
+            // From 1
+            ExtensionMethods.AddJWTAuth(services);
+            // Form 2
+            services.AddJWTAuth();
+        }
+    }
+
+    static class ExtensionMethods
+    {
+        public static IServiceCollection AddJWTAuth(this IServiceCollection services)
+        {
+            // add services into the service collection;
+            return services;
+        }
+    }
+}
