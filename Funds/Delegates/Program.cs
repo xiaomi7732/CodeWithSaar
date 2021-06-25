bc1fd55adc58d0dcb9b2a611ea96be1d5dff9e67
Saar Shen <xiaomi7732@hotmail.com>
2021-06-24 21:31:16 -0700

    Initialize the example

---
+++ Funds/Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
