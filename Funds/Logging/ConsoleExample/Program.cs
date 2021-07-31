3d3988151ed0ae04306aeec8436a3538decce558
Saar Shen <xiaomi7732@hotmail.com>
2021-07-31 08:56:25 -0700

    Basic console logging

--- Funds/Logging/ConsoleExample/Program.cs
+++ Funds/Logging/ConsoleExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleExample
 {
@@ -7,6 +8,21 @@ namespace ConsoleExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // 1. Create a logger factory, register logging providers
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+
+            // 2. Get a logger from the logger factory
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+            // 3. Logging
+            logger.LogInformation("Hello Logger!");
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(intercept: true);
         }
     }
 }
