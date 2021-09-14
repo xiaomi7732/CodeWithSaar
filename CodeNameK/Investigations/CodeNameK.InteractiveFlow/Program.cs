a3df663801e51c0f898ac852704eee0675145810
Saar Shen <xiaomi7732@hotmail.com>
2021-09-13 22:12:59 -0700

    Clean up investigation code

--- CodeNameK/Investigations/CodeNameK.InteractiveFlow/Program.cs
+++ CodeNameK/Investigations/CodeNameK.InteractiveFlow/Program.cs
@@ -13,38 +13,34 @@ namespace CodeNameK.InteractiveFlow
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Interactive flow");
-
             string[] scopes = new[] { "Files.ReadWrite.All" };
-            // string[] scopes = new[] { "User.Read" };
-            string clientId = "45d8bdbc-bd15-4f96-868d-3faf1aa7d34a";
+            string clientId = "c35c09da-4ac5-47e7-98cc-2ba2979d8ae7";
             string tenantId = "common";
-            // string tenantId = "common";
 
             InteractiveBrowserCredentialOptions options = new InteractiveBrowserCredentialOptions
             {
                 TenantId = tenantId,
                 ClientId = clientId,
                 AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-                // RedirectUri = new Uri("https://login.microsoftonline.com/common/oauth2/nativeclient"),
                 RedirectUri = new Uri("http://localhost"),
             };
+            InteractiveBrowserCredential credential = new InteractiveBrowserCredential(options);
+            GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
+            Console.WriteLine("Interactive flow");
+            await UseGraphClient(graphClient).ConfigureAwait(false);
 
-            // IPublicClientApplication publicClientApp = PublicClientApplicationBuilder.Create(clientId)
-            //     .WithDefaultRedirectUri()
-            //     .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
-            //     .Build();
+            DeviceCodeCredential credential2 = new DeviceCodeCredential(new DeviceCodeCredentialOptions
+            {
+                TenantId = tenantId,
+                ClientId = clientId,
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            });
+            Console.WriteLine("DeviceCode flow");
+            await UseGraphClient(graphClient).ConfigureAwait(false);
+        }
 
-            // DeviceCodeCredential dcredential = new DeviceCodeCredential(new DeviceCodeCredentialOptions
-            // {
-            //     TenantId = tenantId,
-            //     ClientId = clientId,
-            //     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-            // });
-
-            InteractiveBrowserCredential interactiveCredential = new InteractiveBrowserCredential(options);
-
-            GraphServiceClient graphClient = new GraphServiceClient(interactiveCredential, scopes);
+        private static async Task UseGraphClient(GraphServiceClient graphClient)
+        {
             User me = await graphClient.Me.Request().GetAsync();
 
             IDriveItemChildrenCollectionPage driveItemPage = await graphClient.Me.Drive.Root.Children.Request().GetAsync().ConfigureAwait(false);
@@ -65,53 +61,6 @@ namespace CodeNameK.InteractiveFlow
                     driveItemPage = null;
                 }
             }
-
-
-            Console.WriteLine("Me: {0}", me.DisplayName);
-            // Console.WriteLine("Drive Name: {0}", driveRoot.);
         }
-
-        // private static async Task<string> SignInUserAndGetTokenUsingMSAL(PublicClientApplicationOptions configuration, string[] scopes)
-        // {
-        //     string authority = string.Concat(configuration.Instance, configuration.TenantId);
-
-        //     // Initialize the MSAL library by building a public client application
-        //     IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder.Create(configuration.ClientId)
-        //                                             .WithAuthority(authority)
-        //                                             .WithDefaultRedirectUri()
-        //                                             .Build();
-
-
-        //     AuthenticationResult result;
-        //     try
-        //     {
-        //         var accounts = await application.GetAccountsAsync();
-        //         result = await application.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
-        //          .ExecuteAsync();
-        //     }
-        //     catch (MsalUiRequiredException ex)
-        //     {
-        //         result = await application.AcquireTokenInteractive(scopes)
-        //          .WithClaims(ex.Claims)
-        //          .ExecuteAsync();
-        //     }
-
-        //     return result.AccessToken;
-        // }
-
-        // /// <summary>
-        // /// Sign in user using MSAL and obtain a token for MS Graph
-        // /// </summary>
-        // /// <returns></returns>
-        // private async static Task<GraphServiceClient> SignInAndInitializeGraphServiceClient(PublicClientApplicationOptions configuration, string[] scopes)
-        // {
-        //     GraphServiceClient graphClient = new GraphServiceClient(MSGraphURL,
-        //         new DelegateAuthenticationProvider(async (requestMessage) =>
-        //         {
-        //             requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingMSAL(configuration, scopes));
-        //         }));
-
-        //     return await Task.FromResult(graphClient);
-        // }
     }
 }
