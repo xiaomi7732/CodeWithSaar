625f10ff2264ac357456301447da6eb60ecd5799
Saar Shen <xiaomi7732@hotmail.com>
2021-10-02 16:05:35 -0700

    Add example of follow SDK

---
+++ CodeNameK/Investigations/CodeNameK.FollowSDKDoc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Graph;
+
+namespace CodeNameK.FollowSDKDoc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] scopes = { "User.Read" };
+            string clientId = "c35c09da-4ac5-47e7-98cc-2ba2979d8ae7";
+            string tenantId = "common";
+            InteractiveBrowserCredentialOptions interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = clientId,
+                TenantId = tenantId,
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+                RedirectUri = new Uri("http://localhost"),
+            };
+            InteractiveBrowserCredential interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+            GraphServiceClient graphClient = new GraphServiceClient(interactiveBrowserCredential, scopes); // you can pass the TokenCredential directly to the GraphServiceClient
+
+            User me = await graphClient.Me.Request()
+                            .GetAsync();
+            Console.WriteLine(me.DisplayName);
+        }
+    }
+}
