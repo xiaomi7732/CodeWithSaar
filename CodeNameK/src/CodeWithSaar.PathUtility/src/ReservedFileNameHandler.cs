48940afa700ae91f30bea3c6d84ea748b0b4bd50
Saar Shen <xiaomi7732@hotmail.com>
2021-11-03 21:45:36 -0700

    Do NOT expose implmentation details

--- CodeNameK/src/CodeWithSaar.PathUtility/src/ReservedFileNameHandler.cs
+++ CodeNameK/src/CodeWithSaar.PathUtility/src/ReservedFileNameHandler.cs
@@ -12,7 +12,7 @@ namespace CodeWithSaar
     /// CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9, LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, and LPT9.
     /// Also avoid these names followed immediately by an extension; for example, NUL.txt is not recommended.
     /// </summary>
-    public class ReservedFileNameHandler : IFileNameProcessor
+    internal class ReservedFileNameHandler : IFileNameProcessor
     {
         private readonly char _escapeChar;
 
