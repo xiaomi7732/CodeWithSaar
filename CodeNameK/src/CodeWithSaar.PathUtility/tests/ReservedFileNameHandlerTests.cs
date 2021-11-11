a109ff0a90f9e01e55cc7d680229f16afbb173c5
Saar Shen <xiaomi7732@hotmail.com>
2021-11-11 14:55:34 -0800

    Tweak unit tests a bit for reserved file name handler

--- CodeNameK/src/CodeWithSaar.PathUtility/tests/ReservedFileNameHandlerTests.cs
+++ CodeNameK/src/CodeWithSaar.PathUtility/tests/ReservedFileNameHandlerTests.cs
@@ -7,36 +7,18 @@ namespace CodeWithSaar.FileUtilityUnitTests
         [Theory]
         [InlineData("CON", "%CON")] // Simple case
         [InlineData("cOn", "%cOn")] // Case insensitive
-        [InlineData("%LPT2", "%%LPT2")] // Do NOT over encode
-        [InlineData("CONContainsCON", "CONContainsCON")] // Do NOT partial match
+        [InlineData("%LPT2", "%%LPT2")] // Encode the escaper, but do NOT over encode
+        [InlineData("CONContainsCON", "CONContainsCON")] // Do NOT encode partial match
         [InlineData("CON.con", "%CON.con")] // File with extension
+        [InlineData("COM3.com.txt", "%COM3.com.txt")] // With multiple extensions
         public void ShouldEncode(string input, string expected)
         {
             ReservedFileNameProcessor target = new ReservedFileNameProcessor();
             string actual = target.Encode(input);
             Assert.Equal(expected, actual);
-        }
 
-        [Theory]
-        [InlineData("%CON", "CON")] // Simple case
-        [InlineData("%cON", "cON")] // case insensitive
-        [InlineData("%CON.txt", "CON.txt")] // File with extension
-        public void ShouldDecodeFullMatch(string input, string expected)
-        {
-            ReservedFileNameProcessor target = new ReservedFileNameProcessor();
-            string actual = target.Decode(input);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ShouldAllowUseEscapeCharByUser()
-        {
-            string content = "%CON";
-            ReservedFileNameProcessor target = new ReservedFileNameProcessor();
-            string actual = target.Decode(target.Encode(content));
-            // If the user put in a file name that is same as encoded result, after encode and decode, the user input should not be changed.
-            Assert.Equal(content, actual);
+            string decoded = target.Decode(actual);
+            Assert.Equal(input, decoded);
         }
     }
 }
