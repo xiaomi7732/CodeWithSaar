94f072a4fb88a0527a98144e9cc0e4b438ccdd0b
Saar Shen <xiaomi7732@hotmail.com>
2021-12-26 21:38:39 -0800

    Add channel investigation project

---
+++ CodeNameK/Investigations/CodeNameK.Channels/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+Task? lastDelayTask = Task.CompletedTask;
+Channel<int> channel = Channel.CreateUnbounded<int>();
+_ = Task.Run(SetupReaderAsync);
+
+await Task.Run(async () =>
+{
+    await channel.Writer.WriteAsync(100);
+    await channel.Writer.WriteAsync(200);
+    channel.Writer.Complete();
+});
+
+await channel.Reader.Completion;
+await lastDelayTask;
+
+async Task SetupReaderAsync()
+{
+    while (true)
+    {
+        int result = await channel.Reader.ReadAsync();
+        lastDelayTask = Task.Delay(1000);
+        await lastDelayTask;
+        Console.WriteLine(result);
+    }
+}
