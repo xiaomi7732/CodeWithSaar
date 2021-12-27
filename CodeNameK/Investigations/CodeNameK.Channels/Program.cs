b0811bc6e77e3df6684f8a775cbca91f75e09f72
Saar Shen <xiaomi7732@hotmail.com>
2021-12-26 22:44:47 -0800

    Update channel example: handle quiting

--- CodeNameK/Investigations/CodeNameK.Channels/Program.cs
+++ CodeNameK/Investigations/CodeNameK.Channels/Program.cs
@@ -2,25 +2,45 @@
 
 Task? lastDelayTask = Task.CompletedTask;
 Channel<int> channel = Channel.CreateUnbounded<int>();
-_ = Task.Run(SetupReaderAsync);
+int dataValue = 1;
 
-await Task.Run(async () =>
+using (CancellationTokenSource cts = new CancellationTokenSource())
 {
-    await channel.Writer.WriteAsync(100);
-    await channel.Writer.WriteAsync(200);
-    channel.Writer.Complete();
-});
+    _ = Task.Run(async () => await SetupReaderAsync(cts.Token));
+    _ = Task.Run(async () =>
+    {
+        while (true)
+        {
+            await channel.Writer.WriteAsync(dataValue);
+            Console.WriteLine("In channel: {0}", dataValue++);
+            await Task.Delay(500);
+        }
+    }, cts.Token);
 
-await channel.Reader.Completion;
-await lastDelayTask;
+    Console.WriteLine("Press any key to start quiting ...");
+    Console.ReadKey(intercept: true);
+    channel.Writer.Complete();  // Stop the writing
+    cts.Cancel();   // Stop the reading and processing;
+    await lastDelayTask; // Finish what has been processed.
+    Console.WriteLine("Cancel requested. There are {0} items left in the channel:", channel.Reader.Count);
+    await foreach (var item in channel.Reader.ReadAllAsync())
+    {
+        Console.WriteLine(item);
+    }
+    // Time to persistent the items left
+    
+}
 
-async Task SetupReaderAsync()
+async Task SetupReaderAsync(CancellationToken cancellationToken)
 {
-    while (true)
+    while (await channel.Reader.WaitToReadAsync())
     {
         int result = await channel.Reader.ReadAsync();
         lastDelayTask = Task.Delay(1000);
         await lastDelayTask;
-        Console.WriteLine(result);
+        Console.WriteLine("Processed: {0}", result);
+        cancellationToken.ThrowIfCancellationRequested();
     }
+
+    Console.WriteLine("There are {0} items in the channel.", channel.Reader.Count);
 }
