e74fc2b2d195a8eac30016dcfb4f5f40154229fc
Saar Shen <xiaomi7732@hotmail.com>
2022-01-16 14:53:51 -0800

    Add shorts for comment / uncomment

---
+++ Shorts/CommentUncomment/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Comment: Ctrl + KC");
+Console.WriteLine("Uncomment: Ctrl + KU");
