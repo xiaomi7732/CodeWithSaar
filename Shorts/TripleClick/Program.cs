9ec8a392c0392b758970e73286e96f52c7e3e140
Saar Shen <xiaomi7732@hotmail.com>
2022-02-01 21:02:33 -0800

    Add an example for triple click

---
+++ Shorts/TripleClick/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Triple click");
+Console.WriteLine("Click click click");
+Console.WriteLine("Click x3");
