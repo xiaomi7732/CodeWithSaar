b5d656dfdf1e11f84ce57e03ca09187c4f976c52
Saar Shen <xiaomi7732@hotmail.com>
2022-02-03 20:29:22 -0800

    Update configuration writer to output to proper path

--- CodeNameK/Investigations/CodeNameK.UserSettings/ConfigurationWriter.cs
+++ CodeNameK/Investigations/CodeNameK.UserSettings/ConfigurationWriter.cs
@@ -1,12 +1,20 @@
+using System.Reflection;
 using System.Text.Json;
 
 namespace UserSettingsDemo;
 
 public class ConfigurationWriter
 {
+    private readonly string _userSettingsFilePath;
+
+    public ConfigurationWriter()
+    {
+        _userSettingsFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, UserConfiguration.FileName);
+    }
+
     public async Task WriteConfigurationAsync(UserConfiguration configuration, CancellationToken cancellationToken)
     {
-        using (Stream output = new FileStream(UserConfiguration.FileName, FileMode.Create, FileAccess.Write))
+        using (Stream output = new FileStream(_userSettingsFilePath, FileMode.Create, FileAccess.Write))
         {
             await JsonSerializer.SerializeAsync(output, configuration);
         }
