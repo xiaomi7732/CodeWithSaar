74d6810add9a7184576060c2993c64548d401601
Saar Shen <xiaomi7732@hotmail.com>
2022-03-07 21:48:19 -0800

    Move DI to App

---
+++ CodeNameK/src/CodeNameK.Droid/Logging/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeNameK.Droid.Logging
+{
+    public static class Extensions
+    {
+        //public static ILoggingBuilder AddAndroidLogging(this ILoggingBuilder loggingBuilder)
+        //{
+        //    // loggingBuilder.
+        //}
+    }
+}
--- CodeNameK/src/CodeNameK.Droid/MainActivity.cs
+++ CodeNameK/src/CodeNameK.Droid/MainActivity.cs
@@ -6,16 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using AndroidX.AppCompat.App;
 using AndroidX.AppCompat.Widget;
-using CodeNameK.Contracts.CustomOptions;
-using CodeNameK.DAL;
 using CodeNameK.DAL.Interfaces;
 using CodeNameK.DataContracts;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.IO;
-using AndroidEnvironment = Android.OS.Environment;
 using CodeNameK.Core.Utilities;
 using System.Linq;
 
@@ -24,16 +20,12 @@ namespace CodeNameK.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        private IServiceCollection? _services;
-        private ServiceProvider? _serviceProvider;
+        private IServiceProvider _serviceProvider;
 
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            _services = new ServiceCollection();
-            _services = RegisterServices(_services);
-
+            _serviceProvider = ((App?)Application)?.ServiceProvider ?? throw new InvalidProgramException("Service Provider doesn't exist.");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
@@ -67,9 +59,10 @@ namespace CodeNameK.Droid
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View)sender;
+            IServiceProvider serviceProvider = _serviceProvider;
 
-            ILocalPathProvider localPathProvider = ServiceProvider.GetRequiredService<ILocalPathProvider>();
-            ICategoryRepo categoryRepo = ServiceProvider.GetRequiredService<ICategoryRepo>();
+            ILocalPathProvider localPathProvider = serviceProvider.GetRequiredService<ILocalPathProvider>();
+            ICategoryRepo categoryRepo = serviceProvider.GetRequiredService<ICategoryRepo>();
             string categoryId = "What can go wrong?";
 
             Category? category = categoryRepo.GetAllCategories().FirstOrDefault(c => string.Equals(c.Id, categoryId));
@@ -80,8 +73,9 @@ namespace CodeNameK.Droid
                 Snackbar.Make(view, $"Something went wrong: {onException}", Snackbar.LengthLong);
             });
 
-            IDataPointRepo dataPointRepo = ServiceProvider.GetRequiredService<IDataPointRepo>();
-            dataPointRepo.AddPointAsync(new DataPoint() { 
+            IDataPointRepo dataPointRepo = serviceProvider.GetRequiredService<IDataPointRepo>();
+            dataPointRepo.AddPointAsync(new DataPoint()
+            {
                 Id = Guid.NewGuid(),
                 Category = category,
                 Value = (new Random()).Next(25000),
@@ -97,62 +91,6 @@ namespace CodeNameK.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        private IServiceCollection RegisterServices(IServiceCollection services)
-        {
-#pragma warning disable CS0618 // Type or member is obsolete - for testing only
-            services.AddLogging();
-            services.AddOptions<LocalStoreOptions>().Configure(localStoreOption =>
-            {
-                bool isReadonly = AndroidEnvironment.MediaMountedReadOnly.Equals(AndroidEnvironment.ExternalStorageState);
-                bool isWriteable = AndroidEnvironment.MediaMounted.Equals(AndroidEnvironment.ExternalStorageState);
-
-                if (isReadonly || !isWriteable)
-                {
-                    throw new InvalidOperationException("No good storage.");
-                }
-
-                string? basePath = ApplicationContext?.GetExternalFilesDir(null)?.AbsolutePath;
-                if (string.IsNullOrEmpty(basePath))
-                {
-                    throw new InvalidOperationException("No valid storage for the app.");
-                }
-                // Notice: The device file explorer with Android stuido can't show hidden folder. So, "codeNameK/Data" is used 
-                // instead of ".codeNameK/Data"
-                localStoreOption.DataStorePath = Path.Combine(basePath, "codeNameK/Data");
-            });
-            services.RegisterDataAccessModuleForAndroid();
-#pragma warning restore CS0618 // Type or member is obsolete
-            return services;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            if (disposing)
-            {
-                _serviceProvider?.Dispose();
-                _serviceProvider = null;
-            }
-        }
-
-        private IServiceProvider ServiceProvider
-        {
-            get
-            {
-                if (_serviceProvider is null)
-                {
-                    if (_services is null)
-                    {
-                        throw new InvalidOperationException("Service Collection hasn't been created.");
-                    }
-                    _serviceProvider = _services.BuildServiceProvider();
-                }
-
-                return _serviceProvider;
-            }
         }
     }
 }
