63eef79b38a2c33e40686ceec8a1d3c36b058b55
Saar Shen <xiaomi7732@hotmail.com>
2022-04-03 09:48:15 -0700

    Category list to viewmodel

--- CodeNameK/src/CodeNameK.Droid/CategoryListActivity.cs
+++ CodeNameK/src/CodeNameK.Droid/CategoryListActivity.cs
@@ -3,69 +3,65 @@
 using Android.App;
 using Android.Content;
 using Android.OS;
+using AndroidX.Lifecycle;
 using AndroidX.RecyclerView.Widget;
-using CodeNameK.BIZ.Interfaces;
-using CodeNameK.DataContracts;
+using CodeNameK.Droid.ViewModels;
 using Google.Android.Material.FloatingActionButton;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CodeNameK.Droid
 {
     [Activity(Label = "@string/category_list_activity_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class CategoryListActivity : KActivityBase
     {
-        private List<Category>? _categories;
         private CategoryListAdapter? _adapter;
         private RecyclerView? _recyclerView;
-        private RecyclerView.LayoutManager? _layoutManager;
         private FloatingActionButton? _fab;
         private ILogger? _logger;
+        private CategoryListViewModel? _categoryListViewModel;
 
         protected override void OnCreating(Bundle? savedInstanceState)
         {
             // Create logger
             _logger = GetRequiredService<ILogger<CategoryListActivity>>();
 
-            // Prepare the data source
-            ICategory categoryBiz = GetRequiredService<ICategory>();
-            List<Category> categoryList = _categories = categoryBiz.GetAllCategories().ToList();
-#if DEBUG
-            if (categoryList.Count == 0)
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    _categories.Add(new Category()
-                    {
-                        Id = "Test Category " + (i + 1),
-                    });
-                }
-            }
-#endif
-
-            // Instantiate the adapter and pass in its data source:
-            CategoryListAdapter adapter = _adapter = new CategoryListAdapter(
-                categoryList,
-                GetRequiredService<ILoggerFactory>());
-
             // Set our view from the "categorylist" layout resource:
             SetContentView(Resource.Layout.ActivityCategory);
 
-            // Get our RecyclerView layout:
-            RecyclerView recyclerView = FindViewById<RecyclerView>(Resource.Id.CategoryListRecyclerView) ?? throw new ArgumentException("Not able to find view by id. ", nameof(Resource.Id.CategoryListRecyclerView));
-            _recyclerView = recyclerView;
-
-            // Plug in the linear layout manager:
-            RecyclerView.LayoutManager layoutManager = _layoutManager = new LinearLayoutManager(this);
-            recyclerView.SetLayoutManager(layoutManager);
-
-            // Plug the adapter into the RecyclerView:
-            recyclerView.SetAdapter(adapter);
-
             // FAB
             _fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+
+            // Get our RecyclerView layout:
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.CategoryListRecyclerView) ?? throw new ArgumentException("Not able to find view by id. ", nameof(Resource.Id.CategoryListRecyclerView));
+            // Plug in the linear layout manager:
+            _recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+
+
+            // Setup viewmodel
+            _categoryListViewModel = new ViewModelProvider(this).Get(Java.Lang.Class.FromType(typeof(CategoryListViewModel))) as CategoryListViewModel;
+            if (savedInstanceState is not null)
+            {
+                // Restore
+                _logger.LogInformation("Resotre in OnCreate");
+            }
+            else
+            {
+                _logger.LogInformation("First time in OnCreate");
+            }
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // Instantiate the adapter and pass in its data source:
+            _adapter = new CategoryListAdapter(
+                _categoryListViewModel!.Categories,
+                GetRequiredService<ILoggerFactory>());
+
+            // Plug the adapter into the RecyclerView:
+            _recyclerView!.SetAdapter(_adapter);
         }
 
         protected override void OnCreated(Bundle? savedInstanceState)
---
+++ CodeNameK/src/CodeNameK.Droid/ViewModels/CategoryListViewModel.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+
+using Android.App;
+using AndroidX.Lifecycle;
+using CodeNameK.BIZ.Interfaces;
+using CodeNameK.DataContracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeNameK.Droid.ViewModels
+{
+    internal class CategoryListViewModel : AndroidViewModel
+    {
+        private readonly App _application;
+        private List<Category>? _categories;
+        public CategoryListViewModel(Application application) : base(application)
+        {
+            if (application is not App app)
+            {
+                throw new InvalidCastException("Can't cast Application instance to App instance.");
+            }
+            _application = app;
+        }
+
+        public List<Category> Categories
+        {
+            get
+            {
+                if (_categories is null)
+                {
+                    LoadCategories();
+                }
+                return _categories!;
+            }
+        }
+
+        private void LoadCategories()
+        {
+            ICategory categoryBiz =_application.ServiceProvider.GetRequiredService<ICategory>();
+            _categories = categoryBiz.GetAllCategories().ToList();
+#if DEBUG
+            if (_categories.Count == 0)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    _categories.Add(new Category()
+                    {
+                        Id = "Test Category " + (i + 1),
+                    });
+                }
+            }
+#endif
+        }
+    }
+}
