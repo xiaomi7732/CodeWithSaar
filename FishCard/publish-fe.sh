fd561917230270b5a1afeeecbe0e136617126c87
Saar Shen <xiaomi7732@hotmail.com>
2022-07-20 12:26:53 -0700

    Update publish script to deal with 404

--- FishCard/publish-fe.sh
+++ FishCard/publish-fe.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dotnet build ./src/Frontend -c Release --no-incremental
 dotnet publish ./src/Frontend -c Release -o fe-release --no-build
+cp ./fe-release/wwwroot/index.html ./fe-release/wwwroot/404.html
 cp ./fe-release/wwwroot/* ../../FishCard -r
 touch ../../FishCard/.nojekyll
