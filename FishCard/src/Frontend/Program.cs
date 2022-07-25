0ae0e11dafd0209046c3a1e7c8651c845442bb12
Saar Shen <xiaomi7732@hotmail.com>
2022-07-25 09:47:55 -0700

    Move data to json

--- FishCard/src/Frontend/Program.cs
+++ FishCard/src/Frontend/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton<QueryFishService>();
-builder.Services.AddSingleton<RecommendService>();
+builder.Services.AddScoped<QueryFishService>();
+builder.Services.AddScoped<RecommendService>();
 
 await builder.Build().RunAsync();
