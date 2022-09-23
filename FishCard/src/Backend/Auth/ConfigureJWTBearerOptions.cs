8ff61445b8df514f872d24754db74f651f320e32
Saar Shen <xiaomi7732@hotmail.com>
2022-09-23 12:50:35 -0700

    Clean up ConfigureJWTBearerOptions a bit

--- FishCard/src/Backend/Auth/ConfigureJWTBearerOptions.cs
+++ FishCard/src/Backend/Auth/ConfigureJWTBearerOptions.cs
@@ -9,25 +9,23 @@ namespace CodeWithSaar.FishCard.Auth;
 public class ConfigureJWTBearerOptions : IConfigureNamedOptions<JwtBearerOptions>
 {
     private readonly AuthOptions _options;
-    private readonly ILogger<ConfigureJWTBearerOptions> _logger;
+    private readonly ILogger _logger;
 
     public ConfigureJWTBearerOptions(IOptions<AuthOptions> options, ILogger<ConfigureJWTBearerOptions> logger)
     {
-        Console.WriteLine("456");
-
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
     }
 
     public void Configure(JwtBearerOptions options)
     {
+        _logger.LogInformation("No name Configure called of {className}", nameof(ConfigureJWTBearerOptions));
         Configure(JwtBearerDefaults.AuthenticationScheme, options);
     }
 
     public void Configure(string name, JwtBearerOptions options)
     {
-        Console.WriteLine("123");
-        _logger.LogInformation("{name}.{methodName} is called.", nameof(ConfigureJWTBearerOptions), nameof(Configure));
+        _logger.LogInformation("{name}.{methodName} is called. Policy name: {policyName}", nameof(ConfigureJWTBearerOptions), nameof(Configure), name);
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuerSigningKey = true,
