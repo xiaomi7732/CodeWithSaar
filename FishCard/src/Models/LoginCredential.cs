886a4c7eacdc19b61b2b10219ec6b9ea545b8659
Saar Shen <xiaomi7732@hotmail.com>
2022-09-27 20:16:40 -0700

    Add simple validations

--- FishCard/src/Models/LoginCredential.cs
+++ FishCard/src/Models/LoginCredential.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CodeWithSaar.FishCard.Auth;
 
 public class LoginCredential
 {
+    [Required]
     public string UserName { get; set; } = default!;
+    
+    [Required]
     public string Password { get; set; } = default!;
 }
